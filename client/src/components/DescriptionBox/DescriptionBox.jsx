a763aefcb7f52cda83e05e5fbe0bba034a93b4fc
unknown <mdnasifurahman@gmail.com>
2024-07-13 17:14:36 +0600

    clientadd

---
+++ client/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Review(122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+            Step up your style game with our collection of Solid polo shirts. Designed for the fashion-forward, these shirts feature eye-catching patterns and graphics that are sure to turn heads.
+
+            Crafted from a soft and breathable blend of cotton , these shirts are as comfortable as they are stylish. With a classic design, including a ribbed collar and short sleeves, and a three-button placket, these polo shirts are both timeless and on-trend.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
